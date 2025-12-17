0fd6abb2b70c2b457b0991824d5bad0690a539c8
Jota-ato <julioc-01@outlook.com>
2025-12-17 17:22:36 -0600

    Version inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+    return (
+        <>
+            <h1 className="text-center text-5xl my-16 font-bold">Mi hormiguita hemsha ❤️</h1>
+        </>
+    );
+}
+
+export default App;
